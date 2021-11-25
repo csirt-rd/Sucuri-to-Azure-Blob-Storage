62cc3c40612ce8a551b4c6ca3b8ea19e929cb55e
Julio Gonzalez <92810781+jgonzalez-csirtrd@users.noreply.github.com>
2021-11-25 10:26:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
-from os import getcwd, makedirs
+from os import getcwd, path
 from azure.storage.blob import BlobClient
+from azure.core.exceptions import ResourceExistsError
+import threading
 
 # Azure Data 
 AZURE_ACC_KEY = ""
@@ -13,66 +15,45 @@ AZURE_CONN_STR = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};En
 )
 
 # Sucuri Info
+AZURE_TABLE_NAME = ""
 SUCURI_API_URL = "https://waf.sucuri.net/api?v2"
 SUCURI_SITES = []
 
-LOG_FILE = '-'.join([
-    '/'.join([getcwd(), 'logs', 'log']),
-    datetime.now().strftime("%Y%m%d")
-]) + '.txt'
-yesterday = datetime.now() - timedelta(1)
+def daterange(start_date, end_date):    #date range generator
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
 def disk_to_blob():
-    try:   
-        makedirs('/'.join([getcwd(), 'logs']))
-    except FileExistsError:
-        pass
-    for i in SUCURI_SITES:
-        if i["enabled"]:   
-            try:   
-                MY_PATH = '/'.join([
-                    getcwd(),
-                    'sucuri',
-                    i["domain"]
-                ])
-                SUCURI_FILE = '-'.join([
-                    i["domain"],
-                    yesterday.strftime("%Y-%m-%d"),
-                    '1000'
-                ]) + '.csv'  
-                BLOB = '/'.join([i["domain"], SUCURI_FILE])
-                with open(LOG_FILE, 'a', encoding='utf-8') as l:
-                    l.write(
-                        ' '.join([
-                            datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S:%f")[:-3],
-                            '+00:00',
-                            '[INF]',
-                            'Getting csv file from',
-                            i["domain"],
-                            'at',
-                            datetime.now().strftime("%Y-%m-%d"),
-                            '\n'
-                        ])
-                    )
-                    l.close()  
-                with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="sucuri", blob_name=BLOB) as blob:
+    for i in SUCURI_SITES: #iterate through sites dictionaries list
+        if i["enabled"]:    #if site marked as enabled
+            for single_date in daterange(datetime.strptime(i["added_time"], "%Y-%m-%d"), datetime.now()):
+                try:
+                    MY_PATH = '/'.join([
+                        "/mnt/cleonardo",
+                        'sucuri',
+                        i["domain"]
+                    ])
+                    SUCURI_FILE = '-'.join([
+                        i["domain"],
+                        single_date.strftime("%Y-%m-%d"),
+                        '1000'
+                    ]) + '.csv'
                     FILE = '/'.join([MY_PATH,SUCURI_FILE])
-                    with open(FILE, 'rb') as data:
-                        blob.upload_blob(data)
-                with open(LOG_FILE, 'a', encoding='utf-8') as l:
-                    l.write(
-                        ' '.join([
-                            datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S:%f")[:-3],
-                            '+00:00',
-                            '[INF]',
-                            'Sending to',
-                            'Sucuri Blob Storage'
-                            '\n'
-                        ])
-                    )
-                    l.close()  
-            except:
-                continue
+                    if path.isfile(FILE):
+                        with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="sucuri", blob_name=SUCURI_FILE) as blob:
+                            with open(FILE, 'rb') as data:
+                                try:
+                                    blob.upload_blob(data)
+                                except ResourceExistsError:
+                                    pass
+                except:
+                    continue
 
 if __name__ == "__main__":
-    disk_to_blob()
+    threads = list()
+    for index in range(100):
+        x = threading.Thread(target=disk_to_blob, daemon=True)
+        threads.append(x)
+        x.start()
+    for index, thread in enumerate(threads):
+        thread.join()
