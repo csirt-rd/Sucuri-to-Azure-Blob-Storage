abbc57ac412d63b404ca386c009f464fe154a3e1
Julio Gonzalez <92810781+jgonzalez-csirtrd@users.noreply.github.com>
2021-11-29 13:34:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
-from os import getcwd, path
+from os import path, getcwd
+import threading
 from azure.storage.blob import BlobClient
 from azure.core.exceptions import ResourceExistsError
-import threading
 
 # Azure Data 
 AZURE_ACC_KEY = ""
@@ -15,44 +15,36 @@ AZURE_CONN_STR = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};En
 )
 
 # Sucuri Info
-AZURE_TABLE_NAME = ""
 SUCURI_API_URL = "https://waf.sucuri.net/api?v2"
 SUCURI_SITES = []
 
-def daterange(start_date, end_date):    #date range generator
-    for n in range(int ((end_date - start_date).days)):
-        yield start_date + timedelta(n)
-
-def disk_to_blob():
-    for i in SUCURI_SITES: #iterate through sites dictionaries list
-        if i["enabled"]:    #if site marked as enabled
-            for single_date in daterange(datetime.strptime(i["added_time"], "%Y-%m-%d"), datetime.now()):
-                try:
-                    MY_PATH = '/'.join([
-                        "/mnt/cleonardo",
-                        'sucuri',
-                        i["domain"]
-                    ])
-                    SUCURI_FILE = '-'.join([
-                        i["domain"],
-                        single_date.strftime("%Y-%m-%d"),
-                        '1000'
-                    ]) + '.csv'
-                    FILE = '/'.join([MY_PATH,SUCURI_FILE])
-                    if path.isfile(FILE):
-                        with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="sucuri", blob_name=SUCURI_FILE) as blob:
-                            with open(FILE, 'rb') as data:
-                                try:
-                                    blob.upload_blob(data)
-                                except ResourceExistsError:
-                                    pass
-                except:
-                    continue
+#Disk to Azure Storage Blob
+def disk_to_blob(domain, date, enabled):
+    if enabled:
+        PATH = '/'.join([
+            getcwd(),
+            'sucuri',
+            domain
+        ])
+        SUCURI_FILE = '-'.join([
+            domain,
+            date.strftime("%Y-%m-%d"),
+            '1000'
+        ]) + '.csv'
+        FILE = '/'.join([PATH,SUCURI_FILE])
+        if path.isfile(FILE):
+            with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="", blob_name=SUCURI_FILE) as blob:
+                with open(FILE, 'rb') as data:
+                    try:
+                        blob.upload_blob(data)
+                    except ResourceExistsError:
+                        pass
 
 if __name__ == "__main__":
+    yesterday = datetime.now() - timedelta(1)
     threads = list()
-    for index in range(100):
-        x = threading.Thread(target=disk_to_blob, daemon=True)
+    for i in SUCURI_SITES:
+        x = threading.Thread(target=disk_to_blob, args=(i["domain"],yesterday,i["enabled"]), daemon=True)
         threads.append(x)
         x.start()
     for index, thread in enumerate(threads):
