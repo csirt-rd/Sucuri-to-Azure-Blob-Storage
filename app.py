1a15c5d533f1f6eb68239031b1f187a42b41e32d
Julio Gonzalez <92810781+jgonzalez-csirtrd@users.noreply.github.com>
2021-12-08 08:59:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
-from os import path, getcwd
-import threading
+import threading, requests
+import pandas as pd
 from azure.storage.blob import BlobClient
 from azure.core.exceptions import ResourceExistsError
 
@@ -18,33 +18,57 @@ AZURE_CONN_STR = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};En
 SUCURI_API_URL = "https://waf.sucuri.net/api?v2"
 SUCURI_SITES = []
 
-#Disk to Azure Storage Blob
-def disk_to_blob(domain, date):
-    PATH = '/'.join([
-        getcwd(),
-        'sucuri',
-        domain
-    ])
-    SUCURI_FILE = '-'.join([
-        domain,
-        date.strftime("%Y-%m-%d"),
-        '1000'
-    ]) + '.csv'
-    FILE = '/'.join([PATH,SUCURI_FILE])
-    if path.isfile(FILE):
-        with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="", blob_name=SUCURI_FILE) as blob:
-            with open(FILE, 'rb') as data:
+#Sucuri to Azure Storage Blob
+def sucuri_to_blob(domain, key, secret, date):
+    body = requests.post(
+        SUCURI_API_URL,
+        data={
+            "k": key,
+            "s": secret,
+            "a": "audit_trails",
+            "date": date.strftime("%Y-%m-%d"),
+            "format": "json",
+            "limit": 1000
+        }
+    ).json()
+    if len(body) > 6:
+        for o in body:
+            try:
+                o["request_date"] = date.strftime("%d-%b-%Y")
+                o["request_time"] = datetime.now().strftime("%H:%M:%S")
+            except:
+                pass
+            try:
+                del o['geo_location']
+            except KeyError:
+                continue
+            except TypeError:
+                pass
+        try:
+            df = pd.DataFrame(body)
+        except (pd.errors.EmptyDataError, pd.errors.ParserError):
+            pass
+        else:
+            df["site"] = domain
+            df = df[df.is_usable != 0]
+            data = df.to_csv(index=False)
+            BLOB = '-'.join([
+                domain,
+                date.strftime("%Y-%m-%d"),
+                '1000'
+            ]) + '.csv'
+            with BlobClient.from_connection_string(conn_str=AZURE_CONN_STR, container_name="sucuri", blob_name=BLOB) as blob:
                 try:
-                    blob.upload_blob(data)
+                    resp = blob.upload_blob(data)
                 except ResourceExistsError:
                     pass
-
+                
 if __name__ == "__main__":
     yesterday = datetime.now() - timedelta(1)
     threads = list()
     for i in SUCURI_SITES:
         if i["enabled"]:
-            x = threading.Thread(target=disk_to_blob, args=(i["domain"],yesterday), daemon=True)
+            x = threading.Thread(target=sucuri_to_blob, args=(i["domain"],i["key"],i["secret"],yesterday), daemon=True)
             threads.append(x)
             x.start()
     for index, thread in enumerate(threads):
